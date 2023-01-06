8c7ac32ae9a365b612265ea10ef58796a2e9abf3
Derrick Cui <derrick_cui@berkeley.edu>
2023-01-05 23:09:40 -0500

    ass

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -62,93 +62,93 @@ request.get('/tracksShort', (error, response, body) => {
 
 }
 
-const mediume = () => {
-    const grid = document.querySelector('.grid');
-    grid.style.display = "grid";
+// const mediume = () => {
+//     const grid = document.querySelector('.grid');
+//     grid.style.display = "grid";
 
-    const info = document.querySelector('.info');
-    info.style.display = "grid";
+//     const info = document.querySelector('.info');
+//     info.style.display = "grid";
 
-    document.getElementById("banner").style.display = "none";
+//     document.getElementById("banner").style.display = "none";
 
-    const h3Element = document.querySelector("h3");
-    h3Element.innerHTML = `Your top songs of the last 6 months`;
+//     const h3Element = document.querySelector("h3");
+//     h3Element.innerHTML = `Your top songs of the last 6 months`;
       
-        // Add the center element back to the grid
-    grid.innerHTML = `<div class="center">Your top 50 songs of the last 6 months</div>`;
+//         // Add the center element back to the grid
+//     grid.innerHTML = `<div class="center">Your top 50 songs of the last 6 months</div>`;
 
-    // Load top songs for this month
-    let topSongsTable = document.querySelector("tbody");
-    topSongsTable.innerHTML = "";
+//     // Load top songs for this month
+//     let topSongsTable = document.querySelector("tbody");
+//     topSongsTable.innerHTML = "";
 
-    fetch('https://node-express-vercel-sigma.vercel.app/tracksMedium')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            let tracksMedium = data.tracksMedium;
-            // Store the data in Local Storage
-            let j = 0;
-            for (let i = 0; i < 49; i++) {
+//     fetch('https://node-express-vercel-sigma.vercel.app/tracksMedium')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             let tracksMedium = data.tracksMedium;
+//             // Store the data in Local Storage
+//             let j = 0;
+//             for (let i = 0; i < 49; i++) {
 
-                let song = tracksMedium[i];
-                let row = document.createElement("tr");
-                row.innerHTML = `<td>${song.name}</td><td>${song.album.name}</td><td>${song.artists[0].name}</td>`;
-                topSongsTable.appendChild(row);
+//                 let song = tracksMedium[i];
+//                 let row = document.createElement("tr");
+//                 row.innerHTML = `<td>${song.name}</td><td>${song.album.name}</td><td>${song.artists[0].name}</td>`;
+//                 topSongsTable.appendChild(row);
 
-                if (i == 23 || i == 24 || i == 25) {
-                    j++;
-                    grid.innerHTML += `<div class="center"></div>`;
-                } else {
-                    let imageUrl = tracksMedium[i-j].album.images[1].url;
-                    let imgElement = `<img src="${imageUrl}" alt="track image">`;
-                    grid.innerHTML += imgElement;
-                }
-            }
-        });
+//                 if (i == 23 || i == 24 || i == 25) {
+//                     j++;
+//                     grid.innerHTML += `<div class="center"></div>`;
+//                 } else {
+//                     let imageUrl = tracksMedium[i-j].album.images[1].url;
+//                     let imgElement = `<img src="${imageUrl}" alt="track image">`;
+//                     grid.innerHTML += imgElement;
+//                 }
+//             }
+//         });
 
-    };
+//     };
 
-const longe = () => {
-    const grid = document.querySelector('.grid');
-    grid.style.display = "grid";
+// const longe = () => {
+//     const grid = document.querySelector('.grid');
+//     grid.style.display = "grid";
 
-    const info = document.querySelector('.info');
-    info.style.display = "grid";
+//     const info = document.querySelector('.info');
+//     info.style.display = "grid";
 
-    document.getElementById("banner").style.display = "none";
+//     document.getElementById("banner").style.display = "none";
 
-    const h3Element = document.querySelector("h3");
-    h3Element.innerHTML = `Your top 50 songs of all time`;
+//     const h3Element = document.querySelector("h3");
+//     h3Element.innerHTML = `Your top 50 songs of all time`;
 
-    // Load top songs for this month
-    grid.innerHTML = `<div class="center">Your top songs of all time</div>`;
+//     // Load top songs for this month
+//     grid.innerHTML = `<div class="center">Your top songs of all time</div>`;
 
-    // Load top songs for this month
-    let topSongsTable = document.querySelector("tbody");
-    topSongsTable.innerHTML = "";
+//     // Load top songs for this month
+//     let topSongsTable = document.querySelector("tbody");
+//     topSongsTable.innerHTML = "";
 
-    fetch('https://node-express-vercel-sigma.vercel.app/tracksLong')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            let tracksLong = data.tracksLong;
-            // Store the data in Local Storage
-            let j = 0;
-            for (let i = 0; i < 49; i++) {
+//     fetch('https://node-express-vercel-sigma.vercel.app/tracksLong')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             let tracksLong = data.tracksLong;
+//             // Store the data in Local Storage
+//             let j = 0;
+//             for (let i = 0; i < 49; i++) {
 
-                let song = tracksLong[i];
-                let row = document.createElement("tr");
-                row.innerHTML = `<td>${song.name}</td><td>${song.album.name}</td><td>${song.artists[0].name}</td>`;
-                topSongsTable.appendChild(row);
+//                 let song = tracksLong[i];
+//                 let row = document.createElement("tr");
+//                 row.innerHTML = `<td>${song.name}</td><td>${song.album.name}</td><td>${song.artists[0].name}</td>`;
+//                 topSongsTable.appendChild(row);
 
-                if (i == 23 || i == 24 || i == 25) {
-                    j++;
-                    grid.innerHTML += `<div class="center"></div>`;
-                } else {
-                    let imageUrl = tracksLong[i-j].album.images[1].url;
-                    let imgElement = `<img src="${imageUrl}" alt="track image">`;
-                    grid.innerHTML += imgElement;
-                }
-            }
-        });
-};
+//                 if (i == 23 || i == 24 || i == 25) {
+//                     j++;
+//                     grid.innerHTML += `<div class="center"></div>`;
+//                 } else {
+//                     let imageUrl = tracksLong[i-j].album.images[1].url;
+//                     let imgElement = `<img src="${imageUrl}" alt="track image">`;
+//                     grid.innerHTML += imgElement;
+//                 }
+//             }
+//         });
+// };
