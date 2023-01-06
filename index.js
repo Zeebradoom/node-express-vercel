e880ba12dcbb2d3f6c178b9e1b412c9a253c79be
Derrick Cui <derrick_cui@berkeley.edu>
2023-01-05 21:36:00 -0500

    oops

--- index.js
+++ index.js
@@ -101,5 +101,5 @@ res.sendFile(path.join(__dirname, "frontend", "dashboard.html"));
 // });
 
 // // connection
-// const port = process.env.PORT || 9001;
-// app.listen(port, () => console.log(`Listening to port ${port}`));
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
