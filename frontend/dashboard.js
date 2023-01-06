8d8709463dc658ee41a02e77b19fea69ad700d0b
Derrick Cui <derrick_cui@berkeley.edu>
2023-01-05 20:31:28 -0500

    worky??

---
+++ frontend/dashboard.js
@@ -0,0 +1,151 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Check if the document is ready
+//   if (document.readyState === "complete") {
+    // Access and manipulate the element
+    const grid = document.querySelector('.grid');
+    grid.style.display = "none";
+    const info = document.querySelector('.info');
+    info.style.display = "none";
+});
+
+const shorte = () => {
+    // Get the current month and year
+    const date = new Date();
+    const month = date.toLocaleString("en-US", { month: "long" });
+    const year = date.getFullYear();
+
+    const grid = document.querySelector('.grid');
+	grid.style.display = "grid";
+
+    const info = document.querySelector('.info');
+    info.style.display = "grid";
+    
+    document.getElementById("banner").style.display = "none";
+
+    const h3Element = document.querySelector("h3");
+    h3Element.innerHTML = `Your top 50 songs of ${month} ${year}`;
+    
+
+    // Update the inner HTML of the element with the center class
+    grid.innerHTML = `<div class="center">Your top songs of ${month} ${year}</div>`;
+
+    let topSongsTable = document.querySelector("tbody");
+    topSongsTable.innerHTML = "";
+    
+    //add fetched changes from api backend thing fuck
+    fetch('/tracksShort')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let tracksShort = data.tracksShort;
+            // Store the data in Local Storage
+            let j = 0;
+            for (let i = 0; i < 49; i++) {
+                //info table
+                let song = tracksShort[i];
+                let row = document.createElement("tr");
+                row.innerHTML = `<td>${song.name}</td><td>${song.album.name}</td><td>${song.artists[0].name}</td>`;
+                topSongsTable.appendChild(row);
+
+                if (i == 23 || i == 24 || i == 25) {
+                    j++;
+                    grid.innerHTML += `<div class="center"></div>`;
+                } else {
+                    let imageUrl = tracksShort[i-j].album.images[1].url;
+                    let imgElement = `<img src="${imageUrl}" alt="track image">`;
+                    grid.innerHTML += imgElement;
+                }
+            }
+        });
+}
+
+const mediume = () => {
+    const grid = document.querySelector('.grid');
+    grid.style.display = "grid";
+
+    const info = document.querySelector('.info');
+    info.style.display = "grid";
+
+    document.getElementById("banner").style.display = "none";
+
+    const h3Element = document.querySelector("h3");
+    h3Element.innerHTML = `Your top songs of the last 6 months`;
+      
+        // Add the center element back to the grid
+    grid.innerHTML = `<div class="center">Your top 50 songs of the last 6 months</div>`;
+
+    // Load top songs for this month
+    let topSongsTable = document.querySelector("tbody");
+    topSongsTable.innerHTML = "";
+
+    fetch('http://localhost:3000/tracksMedium')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let tracksMedium = data.tracksMedium;
+            // Store the data in Local Storage
+            let j = 0;
+            for (let i = 0; i < 49; i++) {
+
+                let song = tracksMedium[i];
+                let row = document.createElement("tr");
+                row.innerHTML = `<td>${song.name}</td><td>${song.album.name}</td><td>${song.artists[0].name}</td>`;
+                topSongsTable.appendChild(row);
+
+                if (i == 23 || i == 24 || i == 25) {
+                    j++;
+                    grid.innerHTML += `<div class="center"></div>`;
+                } else {
+                    let imageUrl = tracksMedium[i-j].album.images[1].url;
+                    let imgElement = `<img src="${imageUrl}" alt="track image">`;
+                    grid.innerHTML += imgElement;
+                }
+            }
+        });
+
+    };
+
+const longe = () => {
+    const grid = document.querySelector('.grid');
+    grid.style.display = "grid";
+
+    const info = document.querySelector('.info');
+    info.style.display = "grid";
+
+    document.getElementById("banner").style.display = "none";
+
+    const h3Element = document.querySelector("h3");
+    h3Element.innerHTML = `Your top 50 songs of all time`;
+
+    // Load top songs for this month
+    grid.innerHTML = `<div class="center">Your top songs of all time</div>`;
+
+    // Load top songs for this month
+    let topSongsTable = document.querySelector("tbody");
+    topSongsTable.innerHTML = "";
+
+    fetch('http://localhost:3000/tracksLong')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let tracksLong = data.tracksLong;
+            // Store the data in Local Storage
+            let j = 0;
+            for (let i = 0; i < 49; i++) {
+
+                let song = tracksLong[i];
+                let row = document.createElement("tr");
+                row.innerHTML = `<td>${song.name}</td><td>${song.album.name}</td><td>${song.artists[0].name}</td>`;
+                topSongsTable.appendChild(row);
+
+                if (i == 23 || i == 24 || i == 25) {
+                    j++;
+                    grid.innerHTML += `<div class="center"></div>`;
+                } else {
+                    let imageUrl = tracksLong[i-j].album.images[1].url;
+                    let imgElement = `<img src="${imageUrl}" alt="track image">`;
+                    grid.innerHTML += imgElement;
+                }
+            }
+        });
+};
--- index.js
+++ index.js
@@ -7,12 +7,97 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-// Routes
+const redirect_uri = "/callback"; //whitelisted redirect uri via developer dashboard
+const client_id = "bc138b8d2f814b4d881e26d13cccbf72"; //client id from developer dashboard
+const client_secret = "dd8bea225a2041c28b59846c68adee10"; //client secret from developer dashboard
 
+global.access_token;
+
+// Routes
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "login.html"));
 });
 
+//authorize the user by getting a code
+app.get("/authorize", (req, res) => {
+  var auth_query_parameters = new URLSearchParams({
+    response_type: "code",
+    client_id: client_id,
+    scope: "user-top-read",
+    redirect_uri: redirect_uri,
+  });
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" + auth_query_parameters.toString()
+  );
+});
+
+//get the access token from the code
+app.get("/callback", async (req, res) => {
+  const code = req.query.code;
+
+  var body = new URLSearchParams({
+    code: code,
+    redirect_uri: redirect_uri,
+    grant_type: "authorization_code",
+  });
+
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "post",
+    body: body,
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        Buffer.from(client_id + ":" + client_secret).toString("base64"),
+    },
+  });
+
+  const data = await response.json();
+  global.access_token = data.access_token;
+  
+  res.redirect(`/views/dashboard.html`);
+});
+
+
+//generalized function to get data from the spotify api
+async function getData(endpoint) {
+  const response = await fetch("https://api.spotify.com/v1" + endpoint, {
+    method: "get",
+    headers: {
+      Authorization: "Bearer " + global.access_token,
+    },
+  });
+
+  const data = await response.json();
+  return data;
+}
+
+//get the photos of the top 100 tracks albums
+app.get("/tracksShort", async (req, res) => {
+  const userInfo = await getData("/me");
+  const tracksShort = await getData("/me/top/tracks?time_range=short_term&limit=50");
+
+  res.json({ user: userInfo, 
+    tracksShort: tracksShort.items});
+});
+
+app.get("/tracksMedium", async (req, res) => {
+  const userInfo = await getData("/me");
+  const tracksMedium = await getData("/me/top/tracks?time_range=medium_term&limit=50");
+
+  res.json({ user: userInfo, 
+    tracksMedium: tracksMedium.items});
+});
+
+
+app.get("/tracksLong", async (req, res) => {
+  const userInfo = await getData("/me");
+  const tracksLong = await getData("/me/top/tracks?time_range=long_term&limit=50");
+
+  res.json({ user: userInfo, 
+     tracksLong: tracksLong.items,});
+});
 
 // connection
 const port = process.env.PORT || 9001;
